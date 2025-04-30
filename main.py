865bc507c290ceb0123d6f920ea1c3766e70e4ba
Hasin Javaid <87460377+Hasin02@users.noreply.github.com>
2025-04-30 12:51:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -266,4 +266,4 @@ async def ask_me_anything(request: AskRequest):
 # Run the server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
